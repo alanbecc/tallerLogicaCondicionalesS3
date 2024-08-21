c420490b31f93e17b5393459c055d1f1babd69f8
alanbecc <klog66@hotmail.com>
2024-08-21 12:10:25 -0500

    Ejercicio Numero Uno

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+//Comparar Edad Ejercicio Nº 1
+
+let edad = prompt("Ingrese La edad ");
+
+if (edad >= 18) {
+  document.write("Eres Mayor de Edad");
+} else if (edad < 18) {
+  document.write("Eres Menor de Edad");
+}
+document.write("<br>");
+
+
+
+
+
+
